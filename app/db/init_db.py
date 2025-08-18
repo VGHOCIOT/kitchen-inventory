ba59ed6f8d53769412a16b3381935dc326d46f13
Vlad Hociota <vghociot@edu.uwaterloo.ca>
2025-08-18 03:24:23 -0400

    Revert "fix: removed calling of app.db"
    This reverts commit 4d8fd428ab68a52b67025493565188707a787a18.

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,6 +1,6 @@
 import asyncio
-from base import Base
-from session import engine
+from app.db.base import Base
+from app.db.session import engine
 
 async def init_models():
     async with engine.begin() as conn:
