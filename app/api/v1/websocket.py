43c86d275bf9b18396b4e8b75164d95168d706e0
Vlad Hociota <vghociot@edu.uwaterloo.ca>
2025-11-29 02:12:57 -0500

    fix: added cors and list def in websocket

--- app/api/v1/websocket.py
+++ app/api/v1/websocket.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from typing import List
 
 router = APIRouter()
 
--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints_items import router as items_router
 from app.api.v1.websocket import router as websocket_router
 
 app = FastAPI(title="Kitchen Inventory API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(items_router, prefix="/api/v1/items")
 app.include_router(websocket_router)
